f606fdb4b4dcf9b24a9514b785fb5f833e52f9da
Creator5923 <nlokhande5923@gmail.com>
2020-09-01 08:10:14 -0700

    cover page done
    pointer on hover
    
    minute changes
    
    hover effect added

--- src/components/Home/Cover/Cover.jsx
+++ src/components/Home/Cover/Cover.jsx
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
-import './Cover.css'
+import React, { Component } from "react";
+import "./Cover.css";
 
 class Cover extends Component {
-    render() {
-        return (
-            <div>
-                Cover Page
+  render() {
+    return (
+      <div>
+        <section id="cover-display">
+          <div id="flex-container">
+            <div className="display-head">
+              <h1 className="display-heading">
+                Technical Blogging <br />
+                website for posting <br />
+                Technical blogs
+              </h1>
+              <p className="good-line">
+                A random good line for the good work
+                <br /> of coding club and this website
+              </p>
+              <div className="inf-btn">
+                <button className="trans-btn">Know us</button>
+                <button className="trans-btn">Contact Us</button>
+              </div>
             </div>
-        );
-    }
+            <img
+              className="coder-image"
+              src="./media/coder.png"
+              alt="coder"
+            ></img>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Cover;
