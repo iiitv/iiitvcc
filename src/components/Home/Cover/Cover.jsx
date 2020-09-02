2400a9d5016673a6d8509a89c1cf71c4de66e0d8
Creator5923 <nlokhande5923@gmail.com>
2020-09-02 04:39:34 -0700

    minute changes

--- src/components/Home/Cover/Cover.jsx
+++ src/components/Home/Cover/Cover.jsx
@@ -4,32 +4,31 @@ import "./Cover.css";
 class Cover extends Component {
   render() {
     return (
-        <div>
+      <div>
         <section id="cover-display">
-        <div id="flex-container">
-          <div className="display-head">
-            <h1 className="display-heading">
-              Technical Blogging <br />
-              website for posting <br />
-              Technical blogs
-            </h1>
-            <p className="good-line">
-              A random good line for the good work
-              <br /> of coding club and this website
-            </p>
-            <div className="inf-btn">
-              <button className="trans-btn">Know us</button>
-              <button className="trans-btn">Contact Us</button>
+          <div id="flex-container">
+            <div className="display-head">
+              <h1 className="display-heading">
+                Technical Blogging <br />
+                website for posting <br />
+                Technical blogs
+              </h1>
+              <p className="good-line">
+                A random good line for the good work
+                <br /> of coding club and this website
+              </p>
+              <div className="inf-btn">
+                <button className="trans-btn">Know us</button>
+                <button className="trans-btn">Contact Us</button>
+              </div>
             </div>
+            <img
+              className="coder-image"
+              src="./media/coder.png"
+              alt="coder"
+            ></img>
           </div>
-          <img
-            className="coder-image"
-            src="./media/coder.png"
-            alt="coder"
-          >
-          </img>
-        </div>
-      </section>
+        </section>
       </div>
     );
   }
