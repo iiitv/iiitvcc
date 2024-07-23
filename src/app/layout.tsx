adfa30f312b816859fb7c1f1c9983972764f8397
Devyash Saini <dysaini2004@gmail.com>
2024-07-23 22:32:31 +0530

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Loading from "@/components/loading";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Suspense fallback={<Loading />}>{children}</Suspense>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
