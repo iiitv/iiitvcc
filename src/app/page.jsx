f0be92f74708b1d53a66ba9e457b1d0af2b24bf4
Devesh Sawant <deveshsawant05@gmail.com>
2024-07-26 17:03:23 +0530

    Added Navbar component to Layout

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+function Home() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Home;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex justify-center item-center ">
-      <h1>Home</h1>
-    </div>
-  );
-}
