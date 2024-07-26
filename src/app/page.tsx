20607ff5740cec35e1c3ea30934c0ec6eba7de47
Devesh Sawant <deveshsawant05@gmail.com>
2024-07-26 18:51:59 +0530

    Fixed Navbar overlapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center item-center ">
-      <h1>Home </h1>
+      <h1>Home</h1>
     </div>
   );
 }
