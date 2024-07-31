71b5510bea8b52cff6e76fa1aad7664a866f0bb2
Devyash Saini <dysaini2004@gmail.com>
2024-07-31 22:26:22 +0530

    one more attempt to set OG

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
-import Head from 'next/head';
+import type { Metadata } from "next";
 
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
+export const metadata: Metadata = {
+  title: "Coding Club IIITV",
+  description: "IIITV Coding Club Community Website | Workshops | Mentorship | Community",
+  openGraph: {
+    title: 'Coding Club IIITV',
+    description: 'IIITV Coding Club Community Website | Workshops | Mentorship | Community',
+    images: ["https://iiitvcc.vercel.app/LinkPreview.png"],
+    type: 'website',
+    locale: 'en_IN',
+    url: 'https://iiitvcc.vercel.app',
+  },
+};
+
 export default function RootLayout({ children } : RootLayoutProps) {
   return (
     <>
-        <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta property="og:title" content="Coding Club IIITV" />
-        <meta property="og:site_name" content="IIITVCC" />
-        <meta property="og:url" content="https://iiitvcc.vercel.app" />
-        <meta property="og:description" content="IIITVCC Community Website | Workshops | Mentorship | Community" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://iiitvcc.vercel.app/LinkPreview.png" />
-      </Head>
       <Navbar />
       {children}
       <Footer />
