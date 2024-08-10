8543e3f57a5d9248167a6457f27bd4ac716aef0e
jenilparmar <jerryhash231@gmail.com>
2024-08-10 21:48:12 +0530

    dynamecally code with api use

--- src/app/event/page.jsx
+++ src/app/event/page.jsx
@@ -1,37 +1,63 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useEffect, useState } from "react";
 import EventBox from "../../components/ui/EventBox";
-function Page() {
-//////////////////// if Other category box which is smaller just pass secondCat///////////////////
-  return (
-    <>
-      <div className="flex flex-col w-full h-full">
-        <div className="self-center h-fit py-2 w-11/12 flex flex-col bg-[#201f31] mt-4">
-          <p className="pl-12 pt-5 text-2xl font-medium">Events Near</p>
-          <div className="self-center  w-full flex flex-row">
-            <section className="h-fit w-1/3 gap-8 py-4 flex flex-col pl-12">
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"} secondCat/>
-            </section>
-            <section className="h-fit w-1/3 gap-8 py-4 flex flex-col pl-12">
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"} secondCat/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            </section>
-            <section className="h-fit w-1/3 gap-8 py-4 flex flex-col pl-12">
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}/>
-            <EventBox caption={"Learning from Github"} time={"17 july 2023 | 19:00"} category={"WorkShop"}  secondCat/>
-            </section>
 
-          </div>
-          <button className="text-xl text-[#FFBADE] border-[#FFBADE] border-[0.5vh] w-fit px-32 active:scale-95 transition-all duration-100 self-center rounded-3xl py-3 "> Show More </button>
+function Page() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch("/api/v1/get/events");
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  const columns = [[], [], []];
+
+  events.forEach((event, index) => {
+    if (!(index % 4 === 3)) {
+      columns[index % 3].push(
+        <EventBox
+          key={event.id}
+          caption={event.description}
+          time={event.date}
+          category={event.name}
+          hostLink={event.host_link}
+        />
+      );
+    } else {
+      columns[index % 3].push(
+        <EventBox key={event.id} secondCat={index % 4 === 3} />
+      );
+    }
+  });
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="self-center h-fit py-2 w-11/12 flex flex-col bg-[#201f31] mt-4">
+        <p className=" md:pl-12 pt-5 text-2xl font-medium">Events Near</p>
+        <div className="self-center w-full grid grid-flow-row grid-cols-2 md:grid-cols-3">
+          {columns.map((column, colIndex) => (
+            <section
+              key={colIndex}
+              className="h-fit w-full md:w-full gap-8 py-4 grid grid-flow-row  md:pl-12"
+            >
+              {column}
+            </section>
+          ))}
         </div>
+        <button className="text-xl text-[#FFBADE] border-[#FFBADE] border-[0.5vh] w-fit px-32 active:scale-95 transition-all duration-100 self-center rounded-3xl py-3">
+          Show More
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
