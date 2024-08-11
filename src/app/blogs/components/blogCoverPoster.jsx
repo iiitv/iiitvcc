5b74f67c5439563bd7c1c590381e2dd609784121
deveshsawant05 <146441689+deveshsawant05@users.noreply.github.com>
2024-08-11 22:11:03 +0530

    Added Fade In to Blogs Cover Poster, Some Fixes (#89)
    IDK

--- src/app/blogs/components/blogCoverPoster.jsx
+++ src/app/blogs/components/blogCoverPoster.jsx
@@ -3,8 +3,12 @@ import Image from 'next/image';
 export default function Blogs() {
     const blogCoverImage = "/blog_cover_poster.png";
     return(
-        <div className='lg:100vh w-full'>
-            <img src={blogCoverImage} className='lg:100vh w-full' alt="Blog cover poster" />
+        <div className='w-full'>
+            <img src={blogCoverImage} 
+            style={
+                {animation: "pulse-poster 0.5s  cubic-bezier(0.4, 0, 0.6, 1) 1"}
+            }
+            className='w-full' alt="Blog cover poster" />
         </div>
     )
 }
