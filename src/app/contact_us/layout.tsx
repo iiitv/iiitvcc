3fa974aa66a28384ba2073c30f409ce4f7be37de
deveshsawant05 <146441689+deveshsawant05@users.noreply.github.com>
2024-08-14 20:56:02 +0530

    Added Contact Us page (#101)
    Contact Us Page

---
+++ src/app/contact_us/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
