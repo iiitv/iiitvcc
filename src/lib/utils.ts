f52a3ad3c4e026fcf78e35ab5657d66f7fdfc581
deveshsawant05 <146441689+deveshsawant05@users.noreply.github.com>
2024-08-17 00:41:08 +0530

    Jenil's changes (#105)
    Fixed things

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getPublicUrl = (path: string) => {
-  return `https://${process.env.PROJECT_ID}.supabase.co/storage/v1/object/public/${process.env.BUCKET}${path}`;
+  return `https://${process.env.NEXT_PUBLIC_PROJECT_ID}.supabase.co/storage/v1/object/public/${process.env.NEXT_PUBLIC_BUCKET}${path}`;
 };
